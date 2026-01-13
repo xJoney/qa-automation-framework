56a6416885a5a6d9dc5026894495a5d57a589c7a
Jonathan <jonathanhua@hotmail.com>
2026-01-12 23:31:01 -0500

    feat: added get error function

--- ui_tests/pages/login_page.py
+++ ui_tests/pages/login_page.py
@@ -6,6 +6,7 @@ class LoginPage:
         self.username = (By.ID, "user-name")
         self.password = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
+        self.error_message = (By.CSS_SELECTOR, "h3[data-test='error']")
     
     def open(self):
         self.driver.get("https://www.saucedemo.com/")
@@ -14,3 +15,11 @@ class LoginPage:
         self.driver.find_element(*self.username).send_keys(username)
         self.driver.find_element(*self.password).send_keys(password)
         self.driver.find_element(*self.login_button).click()
+
+
+    # if message exist, return message
+    def get_error_message(self):
+        try:
+            return self.driver.find_element(*self.error_message).text
+        except:
+            return ""
